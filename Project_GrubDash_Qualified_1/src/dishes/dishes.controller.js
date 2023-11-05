47e4b7192f99ba2601af3982f68e2cc8b0be2852
wey <pukaimakaola@gmail.com>
2023-11-04 21:15:54 -0400

    passes all router tests

--- Project_GrubDash_Qualified_1/src/dishes/dishes.controller.js
+++ Project_GrubDash_Qualified_1/src/dishes/dishes.controller.js
@@ -7,3 +7,103 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res) {
+    res.json({data: dishes});
+}
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+        const { data = {} } = req.body;
+        if(data[propertyName]) {
+            return next();
+        }
+        next({
+            status: 400,
+            message: `${propertyName} is missing`
+        })
+    }
+}
+
+function hasValidPrice(req, res, next) {
+    const { data: { price } = {} } = req.body;
+    if (price < 0 || !Number.isInteger(price)){
+        return next({
+            status: 400,
+            message: "price less than $0"
+        });
+    }
+    next();
+}
+
+function create(req, res, next) {
+    const { data: { name, description, price, image_url} = {} } = req.body;
+    const newDish = {
+        id: nextId(),
+        name, description, price, image_url,
+    };
+    dishes.push(newDish);
+    res.status(201).json({data: newDish});
+}
+
+function dishExists(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id == dishId);
+    if (foundDish){
+        res.locals.dish = foundDish;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `Dish does not exist: ${dishId}.`
+    })
+}
+
+function read(req, res, next) {
+    res.json({data: res.locals.dish});
+}
+
+function idMatches(req, res, next){
+    const { dishId } = req.params;
+    const { data: {id} = {}} = req.body;
+    if(id && id !== dishId){
+        next({
+        status: 400,
+        message: `doesn't match id ${id}`
+        });
+    }
+    next(); 
+}
+
+function update(req, res, next) {
+    const dish = res.locals.dish;
+    const { data: { name, description, price, image_url} = {} } = req.body;
+
+    // update the dish elements
+    dish.name = name;
+    dish.description = description;
+    dish.price = price;
+    dish.image_url = image_url;
+    // console.log({data: dish})
+    res.json({ data: dish });
+}
+
+module.exports = {
+    list,
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        hasValidPrice,
+        create
+    ],
+    read: [dishExists, read],
+    update: [dishExists, 
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        hasValidPrice,
+        idMatches,
+        update,
+    ],
+}
--- Project_GrubDash_Qualified_1/src/orders/orders.controller.js
+++ Project_GrubDash_Qualified_1/src/orders/orders.controller.js
@@ -7,3 +7,91 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+function list(req, res) {
+    const {dishId} = req.params;
+    res.json({ data: orders.filter(dishId ? order => order.id == dishId : () => true )})
+}
+
+function bodyDataHas(propertyName){
+    return function (req, res, next) {
+        const { data = {} } = req.body;
+        if(data[propertyName]) {
+            return next();
+        }
+        next({
+            status: 400,
+            message: `Missing ${propertyName} in new order`
+        });
+    }
+}
+
+function validDishes(req, res, next) {
+    const { data: { dishes } = {} } = req.body;
+    dishes.forEach((dish) => {
+        if(dish.quantity && Number.isInteger(dish.quantity) && dish.quantity > 0) {
+            next();
+        }
+        next({
+            status: 400,
+            message: "needs a quantity of dishes"
+        });
+    })
+}
+
+function hasValidQuantity(req, res, next) {
+    const {data: {dishes} = {} } = req.body;
+    if(dishes[quantity] && quantity > 0) {
+        return next();
+    }
+    next({
+        status: 400,
+        message: `needs valid quantity`
+    });
+}
+
+function create(req, res, next) {
+    const { data: {deliverTo, mobileNumber, status, dishes} = {} } = req.body;
+    const newOrder = {
+        id: nextId(),
+        deliverTo, mobileNumber, status, dishes,
+    };
+    orders.push(newOrder);
+    res.status(201).json({data: newOrder});
+}
+
+function orderExists(req, res, next) {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id == orderId);
+    if(foundOrder) {
+        res.locals.order = foundOrder;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `No order with Id ${orderId}`
+    });
+}
+
+function read(req, res) {
+    res.json({data: res.locals.order});
+}
+
+function update(req, res, next) {
+    const order = res.locals.dish;
+    const { data: {deliverTo, mobileNumber, status, dishes} = {} } = req.body;
+
+    order.deliverTo = deliverTo;
+}
+
+module.exports = {
+    list,
+    create: [
+        bodyDataHas("deliverTo"),
+        bodyDataHas("mobileNumber"),
+        bodyDataHas("dishes"),
+        validDishes,
+        hasValidQuantity,
+        create
+    ],
+    read: [orderExists, read],
+}
